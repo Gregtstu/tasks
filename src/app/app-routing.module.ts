85c779fd57224d7d6df84c9cb46f55d01c875d27
Gregtstu <boltnev.grisha@yandex.ru>
2022-11-11 13:50:03 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./pages/shared/main-layout/main-layout.component";
+import {NewTasksComponent} from "./pages/new-tasks/new-tasks.component";
+import {AllTasksComponent} from "./pages/all-tasks/all-tasks.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:MainLayoutComponent, children:[
+      {path:'', redirectTo:'/create', pathMatch:'full'},
+      {path:'create', component:NewTasksComponent},
+      {path:'allTasks', component:AllTasksComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/shared/main-layout/main-layout.component';
+import { NewTasksComponent } from './pages/new-tasks/new-tasks.component';
+import { AllTasksComponent } from './pages/all-tasks/all-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    NewTasksComponent,
+    AllTasksComponent
   ],
   imports: [
     BrowserModule,
